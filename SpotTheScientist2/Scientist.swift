d197ed56ffcf97b8643876ec31971d1e0f1d6f9a
Krishna Prasad <praskrishna@gmail.com>
2021-02-15 10:55:54 -0800

    First Commit

---
+++ SpotTheScientist2/Scientist.swift
@@ -0,0 +1,17 @@
+//
+//  Scientist.swift
+//  SpotTheScientist2
+//
+//  Created by Krishna P. Murthy on 2/15/21.
+//
+
+import Foundation
+
+struct Scientist: Decodable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
--- SpotTheScientist2/ViewController.swift
+++ SpotTheScientist2/ViewController.swift
@@ -12,29 +12,29 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var scientists = [String: Scientist]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        loadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "scientists", bundle: nil) else {
+            fatalError("Couldnt load tracking images")
+        }
 
+        configuration.trackingImages = trackingImages
+        
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -45,30 +45,109 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard  let imageAnchor = anchor as? ARImageAnchor else { return nil }
+        
+        guard let name = imageAnchor.referenceImage.name else { return nil }
+        
+        guard  let scientist = scientists[name] else { return nil }
+        
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        
+        plane.firstMaterial?.diffuse.contents = UIColor.clear
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi/2
+        
         let node = SCNNode()
-     
+        node.addChildNode(planeNode)
+        
+        let spacing: Float = 0.005
+        
+        let titleNode = textNode(scientist.name, font: UIFont.boldSystemFont(ofSize: 10))
+        
+        titleNode.pivotOnTopLeft()
+        
+        titleNode.position.x += Float(plane.width/2) + spacing
+        titleNode.position.y += Float(plane.height/2)
+        
+        planeNode.addChildNode(titleNode)
+        
+        let bioNode = textNode(scientist.bio, font: UIFont.systemFont(ofSize: 4), maxWidth: 100)
+        bioNode.pivotOnTopLeft()
+        bioNode.position.x += Float(plane.width/2) + spacing
+        bioNode.position.y += titleNode.position.y - titleNode.heigth - spacing
+        planeNode.addChildNode(bioNode)
+        
+        let flag = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.width/8 * 5)
+        
+        flag.firstMaterial?.diffuse.contents = UIImage(named: scientist.country)
+        
+        let flagNode = SCNNode(geometry: flag)
+        
+        flagNode.pivotOnTopCenter()
+        
+        flagNode.position.y -= Float(plane.height/2) + spacing
+        planeNode.addChildNode(flagNode)
+        
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func loadData() {
+        guard let url = Bundle.main.url(forResource: "scientists", withExtension: "json" ) else {
+            fatalError("Unable to load JSON bundle")
+        }
         
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Unable to load JSON")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loadedScientists = try? decoder.decode([String: Scientist].self, from: data) else {
+            fatalError("Unable to parse JSON")
+        }
+        
+        scientists = loadedScientists
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func textNode(_ str: String, font: UIFont, maxWidth: Int? = nil) -> SCNNode {
+        let text = SCNText(string: str, extrusionDepth: 0)
         
+        text.flatness = 0.1
+        text.font = font
+        
+        if let maxWidth = maxWidth {
+            text.containerFrame = CGRect(origin: .zero, size: CGSize(width: maxWidth, height: 500))
+            text.isWrapped = true
+        }
+        
+        let textNode = SCNNode(geometry: text)
+        
+        textNode.scale = SCNVector3(0.002, 0.002, 0.002)
+        return textNode
+    }
+}
+
+extension SCNNode {
+    var width: Float {
+        return (boundingBox.max.x - boundingBox.min.x) * scale.x
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    var heigth: Float {
+        return (boundingBox.max.y - boundingBox.min.y) * scale.y
+    }
+    
+    func pivotOnTopLeft() {
+        let (min, max) = boundingBox
         
+        pivot = SCNMatrix4MakeTranslation(min.x, (max.y - min.y) + min.y, 0)
+    }
+    
+    func pivotOnTopCenter() {
+        let (min, max) = boundingBox
+        
+        pivot = SCNMatrix4MakeTranslation((max.x - min.x)/2 + min.x, (max.y - min.y) + min.y, 0)
     }
 }
